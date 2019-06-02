d428f5f4fbae48e15d1349a0d6fc8d789507db59
Weslley Braga <wesbragagt@gmail.com>
2019-06-02 10:17:07 -0500

    clearfixed and layout

--- clicky-app/src/components/header.js
+++ clicky-app/src/components/header.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const header = () => (
     <header>
-        <nav className="navbar purple darken-3 text-success fixed-top">
+        <nav className="navbar purple darken-3 text-success sticky-top">
             <ul className="container text-center">
                 <li className="nav-link">Clicky Game</li>
                 <li className="nav-link">Click an Image to begin</li>
                 <li className="nav-link">Score: 0 | Top Score: 0</li>
             </ul>
         </nav>
-
-        <div className="jumbotron jumbotron-fluid text-center p-5 mt-5">
+        
+        <div className="jumbotron-fluid text-center p-5 clearfix">
             <h1 className="display-3">Clicky Game</h1>
             <p className="lead">
                 A game where you are not suppose to click on the same character
